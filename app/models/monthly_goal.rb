1e6b8bdd99dc61393378bfe847788f9e81bef1ff
Kana Masuda <kana0127@example.com>
2026-05-23 16:31:52 +0900

    [14]週目標モデル作成

--- app/models/monthly_goal.rb
+++ app/models/monthly_goal.rb
@@ -2,6 +2,8 @@ class MonthlyGoal < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  has_many :weekly_goals, dependent: :destroy
+
   enum :goal_kind, { step: 0, single: 1 }
 
   validates :title, presence: true
